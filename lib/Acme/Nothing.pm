60585ea7fdcdfce270bc63e79ab25c378501b159
Joshua ben Jore <jjore@cpan.org>
2011-01-24 06:28:06 +0000

    Import of JJORE/Acme-Nothing-0.03 from CPAN.
    gitpan-cpan-distribution: Acme-Nothing
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         JJORE/Acme-Nothing-0.03.tar.gz
    gitpan-cpan-author:       JJORE
    gitpan-cpan-maturity:     released

--- lib/Acme/Nothing.pm
+++ lib/Acme/Nothing.pm
@@ -1,17 +1,17 @@
 package Acme::Nothing;
+BEGIN {
+  $Acme::Nothing::VERSION = '0.03';
+}
+# ABSTRACT: No more module loading!
 
-use warnings;
 use strict;
+use 5.008;
+use warnings;
 
-our $VERSION = '0.02';
-
-open my $fh, '<', \$VERSION;
+open my $fh, '<', \$Acme::Nothing::VERSION;
 close $fh;
 
-@INC = \ &nothing;
-
-sub nothing {
-    local *__ANON__ = $_[1];
+@INC = sub {
     $INC{ $_[1] } = $_[1];
     open my $fh, '<', \!$[ or die;
     return $fh;
@@ -26,65 +26,17 @@ __END__
 
 =head1 NAME
 
-Acme::Nothing - No more module loading !
+Acme::Nothing - No more module loading!
+
+=head1 VERSION
+
+version 0.03
 
 =head1 SYNOPSIS
 
 Stops your script from loading any more modules.
 
     use Acme::Nothing;
-    use Improbable;
-    use Fish;
-
-=head1 AUTHOR
-
-No one.
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-C<bug-acme-nothing at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-Nothing>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Acme::Nothing
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Acme-Nothing>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Acme-Nothing>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-Nothing>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Acme-Nothing>
-
-=back
-
-=head1 ACKNOWLEDGEMENTS
-
-Adam Kennedy, for everything.
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2007 Josh ben Jore, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-=cut
+    use Improbable; # Nope!
+    use Fish;       # Not this either!
+    use CGI;        # Still not loading anything
